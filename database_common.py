301ed4718a77d8109ef2eae0c69c33023232e21d
Silviu Catrina <silviu.catrina@gmail.com>
2020-04-16 20:10:47 +0300

    More changes to database config

--- database_common.py
+++ database_common.py
@@ -48,7 +48,7 @@ def open_database():
 
 def connection_handler(function):
     def wrapper(*args, **kwargs):
-        # connection = open_database()
+        connection = open_database()
         # we set the cursor_factory parameter to return with a RealDictCursor cursor (cursor which provides dictionaries)
         dict_cur = connection.cursor(
             cursor_factory=psycopg2.extras.RealDictCursor)
